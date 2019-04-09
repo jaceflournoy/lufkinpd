31143d6be12043a12b040d0a75e25c10cf29153c
jaceflournoy <jacehflournoy@gmail.com>
2019-04-09 01:04:25 -0500

    RailsAdmin/Language Changes - Massive Amount of Language Changes Sitewide - Re-added railsadmin

--- app/models/txdotstreet.rb
+++ app/models/txdotstreet.rb
@@ -1,2 +1,11 @@
 class Txdotstreet < ApplicationRecord
+
+  def city_to_string
+    city ? 'Yes' : 'No'
+  end
+
+  def state_to_string
+    state ? 'Yes' : 'No'
+  end
+
 end
--- app/models/watersewer.rb
+++ app/models/watersewer.rb
@@ -1,2 +1,19 @@
 class Watersewer < ApplicationRecord
+
+  def wateroff_to_string
+    wateroff ? 'Yes' : 'No'
+  end
+
+  def city_to_string
+    cityleak ? 'Yes' : 'No'
+  end
+
+  def private_to_string
+    privateleak ? 'Yes' : 'No'
+  end
+
+  def no_to_string
+    noleak ? 'There is no leak' : 'N/A'
+  end
+
 end

2e83b2b41c35ed58fb4a47c628030ae7ccf948de
jaceflournoy <jacehflournoy@gmail.com>
2019-04-13 19:16:18 -0500

    Authentication/Authorization - Guests can't delete entries/create users/ access admin panel - Added username and password for admin directory.

--- app/controllers/ithelptickets_controller.rb
+++ app/controllers/ithelptickets_controller.rb
@@ -1,4 +1,5 @@
 class IthelpticketsController < ApplicationController
+  load_and_authorize_resource
   before_action :set_ithelpticket, only: [:show, :edit, :update, :destroy]
 
   # GET /ithelptickets
--- app/controllers/vacationpatrols_controller.rb
+++ app/controllers/vacationpatrols_controller.rb
@@ -1,4 +1,5 @@
 class VacationpatrolsController < ApplicationController
+  load_and_authorize_resource
   before_action :set_vacationpatrol, only: [:show, :edit, :update, :destroy]
 
   # GET /vacationpatrols
